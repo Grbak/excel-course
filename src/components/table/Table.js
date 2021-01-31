3b5d527349050a5e828507f0b22d9bfb661c3c11
Grbak <eden91hazard@gmail.com>
2021-01-31 03:37:01 +0300

    more optimization

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -17,38 +17,52 @@ export class Table extends ExcelComponent {
 
     onMousedown(event) {
         if (event.target.dataset.resize) {
-            // console.log('Start resizing', event.target.dataset.resize)
             const $resizer = $(event.target)
-            // const $parent = $resizer.$el.parentNode // bad
-            // const $parent = $resizer.closest('.column') // still bad
             const $parent = $resizer.closest('[data-type="resizable"]')
             const coords = $parent.getCoords()
-            switch ($resizer.data.resize) {
-                case 'column': {
-                    const cells = this.$root.findAll(`[data-col="${$parent.data.col}"`)
-                    document.onmousemove = e => {
-                        const delta = Math.floor(e.pageX - coords.right)
-                        const value = coords.width + delta
-                        $parent.css({
-                            width: value + 'px'
-                        })
-                        cells.forEach(el => el.style.width = value + 'px')
-                    }
-                    break;
-                }
-                case 'row': {
-                    document.onmousemove = e => {
-                        const delta = Math.floor(e.pageY - coords.bottom)
-                        $parent.css({
-                            height: coords.height + delta + 'px'
-                        })
-                    }
-                    break;
+            const type = $resizer.data.resize
+            const side = type === 'column' ? 'bottom' : 'right'
+            $resizer.css({
+                opacity: 1,
+                [side]: '-5000px'
+            })
+            let value;
+            document.onmousemove = e => {
+                if (type === 'column') {
+                    const delta = Math.floor(e.pageX - coords.right)
+                    value = coords.width + delta
+                    $resizer.css({
+                        right: -delta + 'px',
+                    })
+                } else {
+                    const delta = Math.floor(e.pageY - coords.bottom)
+                    value = coords.height + delta
+                    $resizer.css({
+                        bottom: -delta + 'px',
+                    })
                 }
             }
 
             document.onmouseup = () => {
                 document.onmousemove = null
+                document.onmouseup = null
+                if (type === 'column') {
+                    this.$root
+                        .findAll(`[data-col="${$parent.data.col}"`)
+                        .forEach(el => el.style.width = value + 'px')
+                    $parent.css({
+                        width: value + 'px'
+                    })
+                } else {
+                    $parent.css({
+                        height: value + 'px'
+                    })
+                }
+                $resizer.css({
+                    opacity: 0,
+                    bottom: 0,
+                    right: 0
+                })
             }
         }
     }
