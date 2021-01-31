27d61389d2557049e2e057d8f74830bb433a29c2
Grbak <eden91hazard@gmail.com>
2021-01-31 14:41:16 +0300

    moving logic of resize into a separate class

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,7 +1,8 @@
 /* eslint-disable indent */
 import {ExcelComponent} from '@core/ExcelComponent'
 import {createTable} from './table.template'
-import {$} from '@core/dom'
+// import {$} from '@core/dom'
+import Resize from './Resize'
 
 export class Table extends ExcelComponent {
     static className = 'excel__table'
@@ -17,52 +18,8 @@ export class Table extends ExcelComponent {
 
     onMousedown(event) {
         if (event.target.dataset.resize) {
-            const $resizer = $(event.target)
-            const $parent = $resizer.closest('[data-type="resizable"]')
-            const coords = $parent.getCoords()
-            const type = $resizer.data.resize
-            const side = type === 'column' ? 'bottom' : 'right'
-            $resizer.css({
-                opacity: 1,
-                [side]: '-5000px'
-            })
-            let value;
-                if (type === 'column') {
-                    document.onmousemove = e => {
-                        const delta = Math.floor(e.pageX - coords.right)
-                        value = coords.width + delta
-                        $resizer.css({
-                            right: -delta + 'px',
-                        })
-                    }
-                } else {
-                    document.onmousemove = e => {
-                        const delta = Math.floor(e.pageY - coords.bottom)
-                        value = coords.height + delta
-                        $resizer.css({
-                            bottom: -delta + 'px',
-                        })
-                }
-            }
-
-            document.onmouseup = () => {
-                document.onmousemove = null
-                document.onmouseup = null
-                if (type === 'column') {
-                    this.$root
-                        .findAll(`[data-col="${$parent.data.col}"`)
-                        .forEach(el => el.style.width = value + 'px')
-                } else {
-                    $parent.css({
-                        height: value + 'px'
-                    })
-                }
-                $resizer.css({
-                    opacity: 0,
-                    bottom: 0,
-                    right: 0
-                })
-            }
+            const resize = new Resize(event, this.$root)
+            resize.resize()
         }
     }
 }
